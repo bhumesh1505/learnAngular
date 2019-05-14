c316f1e4a7af1ad63ae547112784a1de2701a6f7
bhumesh1505 <bhumesh1505@gmail.com>
2019-05-14 17:09:19 +0530

    some css

---
+++ try2/js/app.js
@@ -0,0 +1,32 @@
+var routerApp = angular.module('routerApp', ['ui.router']);
+
+routerApp.config(function($stateProvider, $urlRouterProvider) {
+
+    $urlRouterProvider.otherwise('/home');
+
+    $stateProvider
+
+
+    // HOME STATES AND NESTED VIEWS ========================================
+    .state('home', {
+        url: '/home',
+        templateUrl: 'views/partial-home.html'
+    })
+
+    // nested list with custom controller
+    .state('home.list', {
+        url: '/list',
+        templateUrl: 'views/partial-home-list.html',
+        controller: function($scope) {
+            $scope.dogs = ['Bernese', 'Husky', 'Goldendoodle'];
+        }
+    })
+
+    // nested list with just some random string data
+    .state('home.paragraph', {
+        url: '/paragraph',
+        template: 'I could sure use a drink right now.'
+    });
+ 
+
+});
